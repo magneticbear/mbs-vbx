6ed29af92c5dac05e0e05e4dc6288a0fa09f4f62
Jean-Pierre Simard <jp@jpsim.com>
2012-08-08 11:03:32 -0400

    Added Alex and Adrian to the directory

--- app.rb
+++ app.rb
@@ -5,11 +5,13 @@ xJP = "101"
 xRobyn = "102"
 xMohammad = "103"
 xStuart = "104"
+xAlex = "105"
+xAdrian = "106"
 
 respond "/voice" do
   gather = Twilio::Gather.new(:action => "/extension", :numDigits => "3")
-  # gather.addSay "Welcome to Magnetic Bear Studios. Please enter 101 for JP, 102 for Robyn, 103 for Mohammad or 104 for Stuart."
-  gather.addPlay "/Welcome.mp3"
+  gather.addSay "Welcome to Magnetic Bear Studios. Please enter 101 for JP, 102 for Robyn, 103 for Mohammad, 104 for Stuart, 105 for Alex or 106 for Adrian."
+  # gather.addPlay "/Welcome.mp3"
   append gather
   addRedirect "/voice"
 end
@@ -17,21 +19,29 @@ end
 respond "/extension" do
   case params[:Digits]
   when xJP
-    # addSay "Please wait while we connect you to JP"
-    addPlay "/JP.mp3"
+    addSay "Please wait while we connect you to JP"
+    # addPlay "/JP.mp3"
     addDial ENV['JP_PHONE']
   when xRobyn
-    # addSay "Please wait while we connect you to Robyn"
-    addPlay "/Robyn.mp3"
+    addSay "Please wait while we connect you to Robyn"
+    # addPlay "/Robyn.mp3"
     addDial ENV['ROBYN_PHONE']
   when xMohammad
-    # addSay "Please wait while we connect you to Mohammad"
-    addPlay "/Mohammad.mp3"
+    addSay "Please wait while we connect you to Mohammad"
+    # addPlay "/Mohammad.mp3"
     addDial ENV['MOHAMMAD_PHONE']
   when xStuart
-    # addSay "Please wait while we connect you to Stuart"
-    addPlay "/Stuart.mp3"
+    addSay "Please wait while we connect you to Stuart"
+    # addPlay "/Stuart.mp3"
     addDial ENV['STUART_PHONE']
+  when xAlex
+    addSay "Please wait while we connect you to Alex"
+    # addPlay "/Alex.mp3"
+    addDial ENV['ALEX_PHONE']
+  when xAdrian
+    addSay "Please wait while we connect you to Adrian"
+    # addPlay "/Adrian.mp3"
+    addDial ENV['ADRIAN_PHONE']
   else
     addSay "The extension you entered is not valid. Please try again."
     addRedirect "/voice"
